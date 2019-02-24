04d440b9607d82095513013ae848b60318805f54
Derek Cerretani <derekcerretani@gmail.com>
2019-02-24 05:14:32 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -22,12 +22,24 @@ class Board
     puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
   end
 
-  def position(board_location)
-    self.cells[board_location.to_i - 1]
+  def position(cell)
+    self.cells[cell.to_i - 1]
   end
 
   def full?
     self.cells.all? { |p| p != " "}
+  end
+
+  def turn_count
+    self.cells.count { |p| p != " "}
+  end
+
+  def taken?(cell)
+    if self.cells[cell.to_i - 1] != " "
+      true
+    else
+      false
+    end
   end
 
 end
